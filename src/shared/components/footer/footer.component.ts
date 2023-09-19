3733cf63272d7f1eb66cf0ef01a5a527630fa269
WeszeRita <weszelovszkirita@gmail.com>
2023-09-19 18:08:01 +0200

    footer was added

---
+++ src/shared/components/footer/footer.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit{
+  year: number;
+  categoriesLinks = [
+    {
+      title: 'TV & audio',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId1'}
+    },
+    {
+      title: 'Smartphones',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId2'}
+    },
+    {
+      title: 'Laptops & PCs',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId3'}
+    },
+    {
+      title: 'Gadgets',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId4'}
+    },
+    {
+      title: 'Photo & Video',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId5'}
+    },
+    {
+      title: 'Gifts',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId6'}
+    },
+    {
+      title: 'Books',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId7'}
+    },
+    {
+      title: 'Toys',
+      routerLink: '/products',
+      queryParams: {categories: 'categoryId8'}
+    },
+  ];
+  usefulLinks = ['about', 'contact', 'wishlist', 'compare', 'FAQ', 'terms & conditions', 'privacy policy', 'cookie policy'];
+  customerServiceLinks = ['my account', 'my cart', 'track order', 'returns & exchanges', 'repair services', 'support'];
+
+  ngOnInit() {
+    this.year = new Date().getFullYear();
+  }
+}
