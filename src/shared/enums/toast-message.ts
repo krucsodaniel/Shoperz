9858e526b8811957749cc3ccbb918231ab1db688
Daniel Krucso <daniel.krucso@nixs.com>
2023-09-19 10:44:58 +0200

    SH-10 Configured Enum for Toast messages

---
+++ src/shared/enums/toast-message.ts
@@ -0,0 +1,6 @@
+export enum ToastMessage {
+  addedToCart = 'cart.productAddedToCart',
+  amountUpdated = 'cart.productAmountUpdated',
+  alreadyInCart = 'cart.productIsAlreadyInCart',
+  removedFromCart = 'cart.productRemovedFromCart',
+}
