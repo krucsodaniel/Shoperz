ba6816556624fd5a1eb2333a62434bdba33735fb
WeszeRita <weszelovszkirita@gmail.com>
2023-09-21 15:17:42 +0200

    SH-7 Fix: Add new routes for footer

--- src/shared/enums/route.ts
+++ src/shared/enums/route.ts
@@ -3,4 +3,17 @@ export enum Route {
   products = 'products',
   productById = 'products/:id',
   cart = 'cart',
+  about = 'about',
+  contact = 'contact',
+  wishlist = 'wishlist',
+  compare = 'compare',
+  faq = 'faq',
+  terms = 'terms-and-conditions',
+  privacyPolicy = 'privacy-policy',
+  cookiePolicy = 'cookie-policy',
+  account = 'account',
+  trackOrder = 'track-order',
+  returns = 'returns-and-exchanges',
+  repairServices = 'repair-services',
+  support = 'support'
 }
