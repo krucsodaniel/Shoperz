9782785f2cb4746d3779ff73de4f2a474e960de4
WeszeRita <weszelovszkirita@gmail.com>
2023-09-28 17:34:54 +0200

    SH-12 Restore app test file

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,44 @@
+import { AppComponent } from './app.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('AppComponent', () => {
+  let componentUnderTest: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(() => {
+    TestBed
+      .configureTestingModule({
+        declarations: [AppComponent],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    componentUnderTest = fixture.componentInstance;
+  });
+
+  it('should create an instance of the component', () => {
+    // Arrange
+    // Act
+    // Assert
+    expect(componentUnderTest).toBeTruthy();
+  });
+
+  describe('template', () => {
+    it('should render the header', async () => {
+      // Arrange
+      // Act
+      fixture.detectChanges();
+      await fixture.whenRenderingDone();
+
+      const header = fixture.debugElement.query(By.css('.header'));
+
+      // Assert
+      expect(header).toBeTruthy();
+    });
+  });
+});
