61d3192741c598f53c6bec0b054720bf6089377e
Daniel Krucso <daniel.krucso@nixs.com>
2023-10-18 16:37:06 +0200

    SH-5 Created and used FirestoreTimestamp shared type

--- src/shared/models/order.model.ts
+++ src/shared/models/order.model.ts
@@ -1,10 +1,11 @@
 import { ICartItem } from '../models';
 import { OrderStatus } from '../enums';
+import { FirestoreTimestampType } from '../types';
 
 export interface IOrder {
   id?: string,
   products: ICartItem[],
   totalAmount: number,
   status: OrderStatus,
-  timestamp?: { seconds: number, nanoseconds: number },
+  timestamp?: FirestoreTimestampType,
 }
