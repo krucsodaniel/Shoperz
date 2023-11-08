360a6b9202a1390d35d445388b7e1eb01165bd16
Daniel Krucso <daniel.krucso@nixs.com>
2023-11-08 17:24:02 +0100

    Optimized Feedback Effects and refactored corresponding service

--- src/shared/services/feedback/feedback-facade.service.ts
+++ src/shared/services/feedback/feedback-facade.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { FeedbackActions } from '../../store/feedback';
+import { FeedbackActions } from '../../store';
 import { IFeedback } from '../../models';
+import { ActionDispatcherService } from '../action-dispatcher.service';
+import { FeedbackActionEnum } from '../../enums';
 
 @Injectable()
 export class FeedbackFacadeService {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private actionDispatcherService: ActionDispatcherService) {}
 
-  createNewFeedback(feedback: IFeedback): void {
-    this.store.dispatch(FeedbackActions.createFeedback({ feedback }));
+  async createNewFeedback(feedback: IFeedback): Promise<void> {
+    return await this.actionDispatcherService.dispatchAsync(
+      FeedbackActions.createFeedback({ feedback }),
+      FeedbackActionEnum.addFeedback,
+    );
   }
 }
