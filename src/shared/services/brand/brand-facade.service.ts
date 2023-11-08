8095a803d18a8f8bd2a6c1618c410e2ad7d56429
Daniel Krucso <daniel.krucso@nixs.com>
2023-11-08 17:15:22 +0100

    Optimized Brand Effects and refactored corresponding service

--- src/shared/services/brand/brand-facade.service.ts
+++ src/shared/services/brand/brand-facade.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { filter, Observable } from 'rxjs';
-import { IBrand } from '@shared-module';
+import { ActionDispatcherService, BrandActions, IBrand } from '@shared-module';
 import { BrandService } from './brand.service';
 import { Store } from '@ngrx/store';
-import { BrandActions, BrandSelectors } from '../../store';
+import { BrandSelectors } from '../../store';
+import { BrandActionEnum } from '../../enums';
 
 @Injectable()
 export class BrandFacadeService {
-  constructor(private brandService: BrandService, private store: Store) {}
+  constructor(
+    private brandService: BrandService,
+    private store: Store,
+    private actionDispatcherService: ActionDispatcherService,
+    ) {}
 
-  initBrandsState(): void {
-    this.store.dispatch(BrandActions.loadBrands());
+  async initBrandsState(): Promise<void> {
+    return await this.actionDispatcherService.dispatchAsync(
+      BrandActions.loadBrands(),
+      BrandActionEnum.loadBrands,
+    );
   }
 
   getBrands(): Observable<IBrand[]> {
