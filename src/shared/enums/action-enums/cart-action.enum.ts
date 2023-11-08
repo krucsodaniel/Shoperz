41bd23bff6d23d6ad4dd875e68917707a0c004f4
Daniel Krucso <daniel.krucso@nixs.com>
2023-11-08 17:12:38 +0100

    Extended Cart Action enum

--- src/shared/enums/action-enums/cart-action.enum.ts
+++ src/shared/enums/action-enums/cart-action.enum.ts
@@ -1,5 +1,7 @@
 export const enum CartActionEnum {
   loadCart = 'load-cart',
   addCartItem= 'add-cart-item',
+  updateCartItem = 'update-cart-item',
+  clearCart = 'clear-cart',
   deleteCartItem = 'delete-cart-item',
 }
