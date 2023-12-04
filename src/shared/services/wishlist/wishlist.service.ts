1ca12b8e906c228753674c11c25e6ff49e677266
WeszeRita <weszelovszkirita@gmail.com>
2023-12-04 18:03:45 +0100

    SH-14 add wishlist

--- src/shared/services/wishlist/wishlist.service.ts
+++ src/shared/services/wishlist/wishlist.service.ts
@@ -7,29 +7,4 @@ export class WishlistService {
 
   constructor(private firestore: Firestore) {}
 
-  getWishlist() {
-  }
-
-  // addProductToWishlist(id: string): Observable<IProduct> {
-  //   const productRef = doc(this.firestore, `${ FirestoreCollection.products }/${ id }`)
-
-    // return from(updateDoc(productRef, { isOnWishlist: true }))
-    //   .pipe(
-    //     map(() => {
-    //       const updatedItem: IProduct = {
-    //         ...products,
-    //         id: id,
-    //         isOnWishlist: true
-    //       };
-    //
-    //       return updatedItem
-    //     })
-    //   )
-
-  // }
-
-
-  // removeProductFromWishlistById(id: string) {
-  //
-  // }
 }
