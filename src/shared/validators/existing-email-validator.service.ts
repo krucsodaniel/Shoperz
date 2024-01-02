cf34a0242de2fd7c85eef9b19214014074267442
Daniel Krucso <daniel.krucso@nixs.com>
2024-01-02 15:10:02 +0100

    Created Email validators

--- src/shared/validators/existing-email-validator.service.ts
+++ src/shared/validators/existing-email-validator.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { catchError, map, Observable, of } from 'rxjs';
 import { UserService } from '../services';
+import { catchError, map, Observable, of } from 'rxjs';
 import { IUser } from '../models';
 
 @Injectable()
-export class UniqueEmailValidatorService implements AsyncValidator {
+export class ExistingEmailValidatorService implements AsyncValidator {
   constructor(private userService: UserService) {}
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     return this.userService.getUserByEmail(control.value)
       .pipe(
         map((user: IUser) => {
-          return !user
+          return user
             ? null
-            : { uniqueEmail: { isTaken: true } };
+            : { existingEmail: { isExisting: false } };
         }),
-        catchError(() => of({ uniqueEmail: { unknownError: true } })),
+        catchError(() => of({ existingEmail: { unknownError: true } })),
       )
   }
 }
--- src/shared/validators/index.ts
+++ src/shared/validators/index.ts
@@ -1,2 +1,3 @@
 export * from './unique-email-validator.service';
 export * from './password-should-match-validator';
+export * from './existing-email-validator.service';
